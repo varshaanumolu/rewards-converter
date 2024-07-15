7665032e985a36994b8a9c88ed5546ebecfac442
varsh <varsh@DESKTOP-NLA27C1.lan>
2024-07-15 17:12:15 -0400

    test cases

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,18 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+    	int milesValue = 18571;
+    	double cashValue = 65;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
+       
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+    	int milesValue = 1200;
+    	double cashValue = 4.2;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 }
